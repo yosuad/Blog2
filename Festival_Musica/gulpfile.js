36846c1b208102808036f933136ff08c5c0f17ae
yosuad <yosuad@hotmail.com>
2024-05-06 09:25:55 -0500

    avance_conjunto_4

--- Festival_Musica/gulpfile.js
+++ Festival_Musica/gulpfile.js
@@ -1,17 +1,25 @@
-import { src, dest, watch } from "gulp";
+import { src, dest, watch, series } from "gulp";
 import * as dartSass from "sass";
 import gulpsass from "gulp-sass";
 
 const sass = gulpsass(dartSass);
 
+export function js(done) {
+  src("src/js/app.js").pipe(dest("build/js"));
+  done();
+}
+
 export function css(done) {
-  src("src/scss/app.scss")
+  src("src/scss/app.scss", { sourcemaps: true })
     .pipe(sass().on("error", sass.logError))
-    .pipe(dest("build/css"));
+    .pipe(dest("build/css", { sourcemaps: "." }));
 
   done();
 }
 
 export function dev() {
   watch("src/scss/**/*.scss", css);
+  watch("src/js/**/*.js", js);
 }
+
+export default series(js, css, dev);
